f57b632e45577873a134865ae19920ede44a2be3
Mohamed Singer <mohamed.singer00@gmail.com>
2021-11-06 19:45:46 +0200

    moved navbar code to App.js & untested routing in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,64 @@
 
-import './App.css';
-import Navbar from './components/navbar/Navbar.js';
-import View from "./pages/navbar/viewFlights"
-import Register from "./pages/navbar/Register"
-import Home from "./pages/navbar/Home"
-import Login from "./pages/navbar/Login"
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+import AddFlight from "./components/add-flight";
+import EditFlight from "./components/edit-flight";
+import Flight from "./components/flights";
+import FlightsList from "./components/flights-list";
+
 function App() {
+  const [user, setUser] = React.useState(null);
+
   return (
-      <div className="App">
-        <Navbar/>
-        <Routes>
-        <Route path='/'  element={Home} />
-        <Route path='/viewFlights' element={View} />
-        <Route path='/login' element={Login} />
-        <Route path='/register' element={Register} />
-      </Routes>
+    <div>
+    <nav className="navbar navbar-expand navbar-dark bg-dark">
+      <a href="/" className="navbar-brand">
+        Airline
+      </a>
+      <div className="navbar-nav mr-auto">
+        <li className="nav-item">
+          <Link to={"/flights"} className="nav-link">
+            Flights
+          </Link>
+        </li>
+        <li className="nav-item" >
+                   
+          <Link to={"/"} className="nav-link">
+            Login
+          </Link>
+
+        </li>
       </div>
+    </nav>
+
+   
+    <div className="container mt-3">
+      <Routes>
+        <Route exact path={["/", "/flights"]} component={FlightsList} />
+        <Route 
+          path="/flights/:id/edit"
+          render={(props) => (
+            <EditFlight {...props} user={user} />
+          )}
+        />
+        <Route 
+          path="/flights/:id"
+          render={(props) => (
+            <Flight {...props} user={user} />
+          )}
+        />
+         <Route 
+          path="/flights/create"
+          render={(props) => (
+            <AddFlight {...props} user={user} />
+          )}
+        />
+      </Routes>
+    </div>
+  </div>
     
   );
 }
