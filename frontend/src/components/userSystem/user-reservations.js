c1f84f8ef3facd5ef896706cd2c563b2e91e08da
Abdelkareem Tarek <a_kareem_2000@hotmail.com>
2021-12-05 01:43:07 +0200

    added cancel reservation

--- frontend/src/components/userSystem/user-reservations.js
+++ frontend/src/components/userSystem/user-reservations.js
@@ -8,7 +8,13 @@ const UserReservations = props => {
     const userid = user._id
     console.log(userid)
     const [reservations, setReservations] =useState([]);
-    
+
+    const deleteReservations = (resid) => {
+      ReservationsDataService.delete(resid)
+        .catch(e => {
+          console.log(e);
+        });
+    };
    const retrieveReservations = () => {
         ReservationsDataService.get(userid)
         
@@ -20,7 +26,8 @@ const UserReservations = props => {
             console.log(e);
           });
       };
-      console.log(reservations)
+
+
       useEffect(() => {
         retrieveReservations();
       }, []);
@@ -29,7 +36,7 @@ const UserReservations = props => {
         <div className="row">
         {reservations.map((reservation) => {
           return (
-           <div>
+           <div >
               <div className="col-lg-4 pb-1">
               <div className="card">
                 <div className="card-body">
@@ -39,35 +46,38 @@ const UserReservations = props => {
                   <h5 className="card-title"> Departure Flight</h5>
                   <p className="card-text">
                    <strong>Date: </strong>{reservation.DepartureFlight.Date}<br/>
-                    <strong>Departure Time: </strong>{reservation.DepartureFlight.DepartureTime} <br/> &nbsp;
-                    <strong>Arrival Time: </strong>{reservation.DepartureFlight.ArrivalTime}<br/>
-                    <strong>Flight Number:</strong>{reservation.DepartureFlight._id}<br/>
-                    <strong>Baggage Allowance:</strong>{reservation.DepartureFlight.BaggageAllowance}<br/>
-                    <strong>Trip Duration:</strong>{reservation.DepartureFlight.TripDuration}<br/>
+                    <strong>Departure Time : </strong>{reservation.DepartureFlight.DepartureTime} <br/> &nbsp;
+                    <strong>Arrival Time : </strong>{reservation.DepartureFlight.ArrivalTime}<br/>
+                    <strong>Flight Number : </strong>{reservation.DepartureFlight._id}<br/>
+                    <strong>Baggage Allowance : </strong>{reservation.DepartureFlight.BaggageAllowance}<br/>
+                    <strong>Trip Duration : </strong>{reservation.DepartureFlight.TripDuration}<br/>
+                    <strong>Seats : </strong>{reservation.DepSeats}<br/>
+                    <strong>Cabin Class : </strong>{reservation.CabinClass}<br/>
                     </p>
                     </div>
-                    <div className="card-body">
+                  <div className="card-body">
                   <h5 className="card-title">  Return Flight</h5>
                   <p className="card-text">
                     <strong>Date: </strong>{reservation.ReturnFlight.Date}<br/>
-                    <strong>Departure Time: </strong>{reservation.ReturnFlight.DepartureTime} <br/> &nbsp;
-                    <strong>Arrival Time: </strong>{reservation.ReturnFlight.ArrivalTime}<br/>
-                    <strong>Flight Number:</strong>{reservation.ReturnFlight._id}<br/>
-                    <strong>Baggage Allowance:</strong>{reservation.ReturnFlight.BaggageAllowance}<br/>
-                    <strong>Trip Duration:</strong>{reservation.ReturnFlight.TripDuration}<br/>
+                    <strong>Departure Time : </strong>{reservation.ReturnFlight.DepartureTime} <br/> &nbsp;
+                    <strong>Arrival Time : </strong>{reservation.ReturnFlight.ArrivalTime}<br/>
+                    <strong>Flight Number : </strong>{reservation.ReturnFlight._id}<br/>
+                    <strong>Baggage Allowance : </strong>{reservation.ReturnFlight.BaggageAllowance}<br/>
+                    <strong>Trip Duration : </strong>{reservation.ReturnFlight.TripDuration}<br/>
+                    <strong>Seats : </strong>{reservation.RetSeats}<br/>
+                    <strong>Cabin Class : </strong>{reservation.CabinClass}<br/>
                     </p>
                     </div>
                     <div className="card-body">
-                  <h5 className="card-title">  Return Flight</h5>
+                  <h5 className="card-title">  User info</h5>
                   <p className="card-text">
-                    <strong>First Name :</strong>{reservation.User.firstname}<br/>
-                    <strong>Last Name :</strong>{reservation.User.lastname}<br/>
-                    <strong>Passport Number :</strong>{reservation.User.passportnumber}<br/>
-                    <strong>Email :</strong>{reservation.User.email}<br/>
-                    <strong>Cabin Class</strong>{reservation.CabinClass}<br/>
-                    <strong>Price :</strong>{reservation.Price}<br/>
-                    <strong>Departure Seats</strong>{reservation.DepSeats}<br/>
-                    <strong>Return Seats</strong>{reservation.RetSeats}<br/>
+                    <strong>First Name : </strong>{reservation.User.firstname}<br/>
+                    <strong>Last Name : </strong>{reservation.User.lastname}<br/>
+                    <strong>Passport Number : </strong>{reservation.User.passportnumber}<br/>
+                    <strong>Email : </strong>{reservation.User.email}<br/>
+                    <strong>Total Price : </strong>{reservation.Price}<br/>
+                    <a className="btn btn-success" onClick={() => {if(window.confirm('Are you sure you want to cancel this reservation?')){deleteReservations(reservation.resid)}}}>Cancel Reservation</a> &nbsp;
+                   
                     </p>
                     </div>
                   </p>
