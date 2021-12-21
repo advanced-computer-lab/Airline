ed639fda52d82ec350cf3d2d0bb95e70077785a9
ahmedshamel1 <ahmedshamel1@yahoo.com>
2021-12-21 23:20:01 +0200

    first push

--- frontend/src/components/adminSystem/add-flight.js
+++ frontend/src/components/adminSystem/add-flight.js
@@ -2,6 +2,17 @@ import React, { useState } from "react";
 import{Formik, useFormik} from "formik";
 import * as Yup from "yup";
 import FlightDataService from "../../services/flight";
+import 'react-dropdown/style.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import BackgroundSlider from 'react-background-slider'
+ import image1 from './images/image1.jpg'
+ import image2 from './images/image2.jpg'
+ import image3 from './images/image3.jpg'
+ import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+
 
 export default function Createflight(){
     
@@ -69,185 +80,181 @@ export default function Createflight(){
          },
      });
     return(
-<div>
-<h1 className="card-title">Create New Flight</h1>
-<form onSubmit={formik.handleSubmit}>
-    <div className="row">
-            <div className="row-auto">
-            Flight Number :
-               <input
-               className="form-control"
-                 id="FlightNumber"
-                 name="FlightNumber"
-                 type="text"
-                 placeholder="Flight Number"
-                 onChange={formik.handleChange}
-                value={formik.values.FlightNumber}
-                 />  
 
-           </div>
-           <div className="row-auto">     
+       
+         <div>
+        
+        <div>
+             <BackgroundSlider
+                images={[image1, image2, image3]}
+                duration={4}
+                transition={2} />
+        
+         
+        <h1>Add Flight
+
+        </h1>
+        <Box
+    opacity='[0,0,0]'
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 3, width: '65ch' },
+      }}
+      noValidate
+      textAlign='center'
+      autoComplete="off"
+      height="0px"
+      
+    >
+        
+      <div>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          required
+          id="FlightNumber"
+          label="Flight Number"
+          variant="outlined"
+          value={formik.values.FlightNumber}
+          onChange={formik.handleChange}
+          sx={{background:'white'  }}
+        />
+        <TextField
+        
+          required
+          id=" DepartureTime"
+          label=" Departure Time"
+         // placeholder="Departure Time"
+          type="time"
+          variant="outlined"
+          onChange={formik.handleChange}
+          value={formik.values.DepartureTime}
+          sx={{background:'white'  }}
+        />
+         <div>
+        </div>
+        <TextField
+          required
+          id="ArrivalTime"
+          label="Arrival Time"
+          type="time"
+         // autoComplete="current-password"
+          variant="outlined"
+          onChange={formik.handleChange}
+         value={formik.values.ArrivalTime}
+         sx={{background:'white'  }}
+        />
+        <TextField
+          required
+          id=" Date"
+          label=" Date"
+          type="date"
+          //autoComplete="current-password"
+          variant="outlined"
+          onChange={formik.handleChange}
+         value={formik.values. Date}
+         sx={{background:'white'  }}
+        /><div>
+         </div>
+            <TextField
+          required
+          id=""EconomySeats
+          label="Economy Seats"
+          type="number"
+          variant="outlined"
+          onChange={formik.handleChange}
+         value={formik.values.EconomySeats}
+         sx={{background:'white'  }}
+        />
+        <TextField
+          required
+          id="BusinessSeats"
+          label="Business Seats"
+          type="number"
+          variant="outlined"
+          onChange={formik.handleChange}
+         value={formik.values.BusinessSeats}
+         sx={{background:'white'  }}
+        />
+         <div>
+        </div>
+        <TextField
+          required
+          id=" FirstSeats"
+          label=" First Seats"
+          type="number"
+          variant="outlined"
+          onChange={formik.handleChange}
+          value={formik.values. FirstSeats}
+          sx={{background:'white'  }}
+        />
+        <TextField
+          required
+          id=' DepartureAirport'
+          label=" Departure Airport"
+          color="secondary"
+          variant="outlined"
+          onChange={formik.handleChange}
+          value={formik.values. DepartureAirport}
+          sx={{background:'white'  }}
+        />
+         <div>
+        </div>
+         <TextField
+          required
+          id='DestinationAirport'
+          label="Destination Airport"
+          color="secondary"
+          variant="outlined"
+          onChange={formik.handleChange}
+         value={formik.values.DestinationAirport}
+         sx={{background:'white'  }}
+        />
+         <TextField
+         id='TripDuration'
+          color="secondary"
+          type="number"
+          required
+          variant="outlined"
+          label="Trip Duration"
+          onChange={formik.handleChange}
+         value={formik.values.TripDuration}
+         sx={{background:'white'  }}
+        />
+        <div >
+            </div>
+            <TextField
+          required
+          id=' BaggageAllowed'
+          label=" Baggage Allowed"
+          color="secondary"
+          variant="outlined"
+          onChange={formik.handleChange}
+          value={formik.values.BaggageAllowed}
+          sx={{background:'white'  }}
+        />
+         <TextField
+         id=' Price'
+          color="secondary"
+          type="number"
+          required
+          variant="outlined"
+          label="Trip Duration"
+          onChange={formik.handleChange}
+         value={formik.values. Price}
+         sx={{background:'white'  }}
+        />
+         <div>
+        </div>
            
-           Departure Time : 
-               <input
-               className="form-control"
-                 id="DepartureTime"
-                 name="DepartureTime"
-                 type="time"
-                 placeholder="Departure Time"
-                 onChange={formik.handleChange}
-                value={formik.values.DepartureTime}
-                 />  
-
-           </div>
-           <div className="row-auto"> 
-               
-           Arrival Time :  
-               <input
-               className="form-control"
-                 id="ArrivalTime"
-                 name="ArrivalTime"
-                 type="time"
-                 placeholder="Arrival Time"
-                 onChange={formik.handleChange}
-                value={formik.values.ArrivalTime}
-                 />  
-
-          </div>
-           <div className="row-auto">   
-              
-           Trip Duration :
-               <input
-               className="form-control"
-                 id="TripDuration"
-                 name="TripDuration"
-                 type="text"
-                 placeholder="Trip Duration"
-                 onChange={formik.handleChange}
-                value={formik.values.TripDuration}
-                 />  
-
-          </div>
-           <div className="row-auto">  
-             
-           Date :
-               <input
-               className="form-control"
-                 id="Date"
-                 name="Date"
-                 type="date"
-                 placeholder="Date"
-                 onChange={formik.handleChange}
-                value={formik.values.Date}
-                 />  
-
-            </div>
-           <div className="row-auto">  
-              
-           Economy Seats :
-               <input
-               className="form-control"
-                 id="EconomySeats"
-                 name="EconomySeats"
-                 type="number"
-                 placeholder="Economy Seats"
-                 onChange={formik.handleChange}
-                value={formik.values.EconomySeats}
-                 />  
-
-            </div>
-           <div className="row-auto">  
-              
-           Business Seats :
-               <input
-               className="form-control"
-                 id="BusinessSeats"
-                 name="BusinessSeats"
-                 type="number"
-                 placeholder="Business Seats"
-                 onChange={formik.handleChange}
-                value={formik.values.BusinessSeats}
-                 />  
-
-            </div>
-           <div className="row-auto">    
-            
-           First Seats :
-               <input
-               className="form-control"
-                 id="FirstSeats"
-                 name="FirstSeats"
-                 type="number"
-                 placeholder="First Seats"
-                 onChange={formik.handleChange}
-                value={formik.values.FirstSeats}
-                 />  
-
-            </div>
-           <div className="row-auto">    
-           
-           Departure Airport :
-               <input
-               className="form-control"
-                 id="DepartureAirport"
-                 name="DepartureAirport"
-                 type="text"
-                 placeholder="Departure Airport"
-                 onChange={formik.handleChange}
-                value={formik.values.DepartureAirport}
-                 />  
-
-          </div>
-           <div className="row-auto">      
-            
-           Destination Airport :
-               <input
-               className="form-control"
-                 id="DestinationAirport"
-                 name="DestinationAirport"
-                 type="text"
-                 placeholder="Destination Airport"
-                 onChange={formik.handleChange}
-                value={formik.values.DestinationAirport}
-                 />  
-
-          </div>
-           <div className="row-auto">  
-             
-           Baggage Allowed :
-               <input
-               className="form-control"
-                 id="BaggageAllowed"
-                 name="BaggageAllowed"
-                 type="text"
-                 placeholder="Baggage Allowed"
-                 onChange={formik.handleChange}
-                value={formik.values.BaggageAllowed}
-                 />  
-
-          </div>
-           <div className="row-auto">  
-              
-           Price :
-               <input
-               className="form-control"
-                 id="Price"
-                 name="Price"
-                 type="number"
-                 placeholder="Price"
-                 onChange={formik.handleChange}
-                value={formik.values.Price}
-                 />  
-
-<br/></div>
-
-           
-           <div style={{display: 'flex',  justifyContent:'right', alignItems:'center', height: '5vh'}}>
-           <button type= "submit" class="btn btn-primary">Create</button>
-           </div>
+        <Button variant='contained'  size='large' >Create</Button>
+  
+        
+        </form>
       </div>
-</form>
+     
+      </Box>
+         
 </div>
-
+</div>
     )
 }
